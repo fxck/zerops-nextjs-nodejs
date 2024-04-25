47c200de9c1883772a51c1cb0f4e09b5c9e92bec
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2024-04-26 00:35:52 +0530

    feat: video

--- components/Steps.tsx
+++ components/Steps.tsx
@@ -20,15 +20,11 @@ export function Steps() {
         <h2 className="text-[#292929] text-2xl font-semibold">
           Step 1: Go To Zerops Dashboard{" "}
         </h2>
-<div style={{ padding: '75.63% 0 0 0', position: 'relative' }}>
-  <iframe 
-    src="https://player.vimeo.com/video/939088441?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" 
-    allow="autoplay; fullscreen; picture-in-picture; clipboard-write" 
-    style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%' }} 
-    className="rounded-md">
-  </iframe>
-</div>
-<script src="https://player.vimeo.com/api/player.js"></script>
+        <div>
+          <video width="full" className="rounded-xl mt-4" autoPlay loop>
+            <source src="import.mp4" type="video/mp4" />
+          </video>
+        </div>
         <p className="font-medium text-lg mt-3">
           Click on Import Project button on the sidebar.
         </p>
